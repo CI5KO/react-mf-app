895f7ac01e053336f510a6d4a5150955b0fd1b9a
CI5KO <hector_oliva16k@hotmail.com>
2023-05-17 13:42:07 -0600

    Its Docker Time

--- users/src/App.tsx
+++ users/src/App.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import "./index.scss";
+
 // @ts-ignore
 import Header from "homeComps/Header";
-import Home from "./components/Home";
 
 const App = () => (
   <div>
     <Header />
-    <Home />
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));

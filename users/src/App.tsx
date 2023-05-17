ad25be477c67527e283052bff8b5a3ea6aadc0d8
CI5KO <hector_oliva16k@hotmail.com>
2023-05-17 12:23:48 -0600

    Typescript ignore error

--- users/src/App.tsx
+++ users/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
+// @ts-ignore
 import Header from "homeComps/Header";
 import Home from "./components/Home";
 

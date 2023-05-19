a66f3a1285154715d6601860b124fa35a615b563
CI5KO <hector_oliva16k@hotmail.com>
2023-05-19 09:08:40 -0600

    Email RegEx

--- users/src/modules/domain/userEmail.ts
+++ users/src/modules/domain/userEmail.ts
@@ -1,6 +1,5 @@
 export function isUserEmailValid(email: string): boolean {
-  const EMAIL_REGEX =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
   return EMAIL_REGEX.test(email);
 }

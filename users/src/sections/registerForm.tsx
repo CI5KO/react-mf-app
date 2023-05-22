88c14d88e210f979377a1cce0ce77351ce2facff
CI5KO <hector_oliva16k@hotmail.com>
2023-05-22 07:10:43 -0600

    Register code update

--- users/src/sections/registerForm.tsx
+++ users/src/sections/registerForm.tsx
@@ -1,59 +1,66 @@
 import React, { useState, useEffect } from "react";
+import { createBackEndRepo } from "../modules/infraestructure/backendRepo";
 import { isUserNameValid } from "../modules/domain/userName";
 import { isUserEmailValid } from "../modules/domain/userEmail";
 import { isUserPasswordValid } from "../modules/domain/userPassword";
-
-interface formProps {
-  name: string;
-  email: string;
-  password: string;
-}
 
 export default function RegisterForm() {
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  const [error, setError] = useState<formProps>({
-    name: "",
-    email: "",
-    password: "",
-  });
 
-  const formData: formProps = { name, email, password };
+  const [nameError, setNameError] = useState<string>("");
+  const [emailError, setEmailError] = useState<string>("");
+  const [passwordError, setPasswordError] = useState<string>("");
 
-  useEffect(() => {
-    const isNameValid: boolean = isUserNameValid(name);
-    const isEmailValid: boolean = isUserEmailValid(name);
-    const isPasswordValid: boolean =
-      isUserPasswordValid.containsSpecialCharacter(name);
-    const errors = () => {
-      if (!isNameValid || !isEmailValid || !isPasswordValid)
-        setError({
-          name: isNameValid ? "" : "Name is not valid",
-          email: isEmailValid ? "" : "Email is not valid",
-          password: isPasswordValid ? "" : "Password not valid",
-        });
-    };
-    errors();
-  }, [formData]);
+  const handleClick = () => {
+    createBackEndRepo().create({
+      id: "",
+      name,
+      email,
+      password,
+      isAdmin: false,
+      isFormDone: false,
+    });
+  };
 
   return (
     <div className="grid h-screen">
       <div className="w-1/3 h-1/2 self-center justify-self-center bg-blue-200">
         <h1 className="text-center py-4">Registrarse</h1>
         <div className="grid w-1/2 space-y-4">
-          <input placeholder="Name" onChange={(e) => setName(e.target.value)} />
-          <p>{error?.name ? error?.name : ""}</p>
+          <input
+            placeholder="Name"
+            onChange={(e) => {
+              if (!isUserNameValid(e.target.value))
+                setNameError("Name not Valid!");
+              if (isUserNameValid(e.target.value)) setNameError("");
+              setName(e.target.value);
+            }}
+          />
+          <p>{nameError}</p>
           <input
             placeholder="Email"
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => {
+              if (!isUserEmailValid(e.target.value))
+                setEmailError("Email not valid!");
+              if (isUserEmailValid(e.target.value)) setEmailError("");
+              setEmail(e.target.value);
+            }}
           />
-          <p>{error?.email ? "Error" : ""}</p>
+          <p>{emailError}</p>
           <input
             placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => {
+              if (!isUserPasswordValid.containsSpecialCharacter(e.target.value))
+                setPasswordError("Password not valid!");
+              if (isUserPasswordValid.containsSpecialCharacter(e.target.value))
+                setPasswordError("");
+              setPassword(e.target.value);
+            }}
           />
-          <p>{error?.password ? "Error" : ""}</p>
+          <p>{passwordError}</p>
+          <button onClick={handleClick}>Submit!</button>
         </div>
       </div>
     </div>
